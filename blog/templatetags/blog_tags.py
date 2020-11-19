466fe483e9bdc60e7c5d2f54552c00d4c6821cbe
Trevor <cfc603@users.noreply.github.com>
2020-11-19 10:12:55 -0500

    fix pagination issue with get params
    Squashed commit of the following:
    
    commit 8c3eadf84a3ce87004ed7d6caa806abd61d9fbe6
    Author: Trevor <cfc603@users.noreply.github.com>
    Date:   Thu Nov 19 10:12:30 2020 -0500
    
        update template
    
    commit 750a9e2b603d29fa0dcf7502827979789d5e7697
    Author: Trevor <cfc603@users.noreply.github.com>
    Date:   Thu Nov 19 10:12:18 2020 -0500
    
        add pagination_path templatetag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,6 +5,20 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
+@register.simple_tag(takes_context=True)
+def pagination_path(context, page_num):
+    params = context["request"].GET
+    path = f"?page={page_num}&intro=false"
+    excluded_params = ["page", "intro"]
+
+    if params:
+        for param, value in params.items():
+            if not param in excluded_params:
+                path += f"&{param}={value}"
+
+    return path
+
+
 @register.filter
 def text_primary(value):
     value_list = value.split(" ")
